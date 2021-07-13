dec3068c090596fa14adb9b69c691c7857e64aee
Aleksandr Vishniakov <av@virtoway.com>
2021-07-13 11:10:01 +0200

    VDS-2304: Fix environment variables template

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  platformUrl: 'http://localhost:10645/'
+  production: false
 };
 
 /*
