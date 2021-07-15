d21bb3a03400b04cd4d98d427a630e517bd9692c
Aleksandr Vishniakov <av@virtoway.com>
2021-07-15 16:15:38 +0200

    VDS-2319: Add showcase & customize bootstrap

---
+++ src/app/modules/showcase/showcase-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShowcaseComponent } from './showcase.component';
+
+const routes: Routes = [{ path: '', component: ShowcaseComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ShowcaseRoutingModule { }
