60ec390b2d0aa442ad943765864e731216ffe00f
Aleksandr Vishniakov <av@virtoway.com>
2021-07-15 16:30:27 +0200

    Fix linting rules, fix linted code and fix code smell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'showcase', loadChildren: () => import('./modules/showcase/showcase.module').then(m => m.ShowcaseModule) }];
+const routes: Routes = [
+  { path: 'showcase',
+    loadChildren: () => import('./modules/showcase/showcase.module').then(m => m.ShowcaseModule) },
+];
 
 @NgModule({
   imports: [
