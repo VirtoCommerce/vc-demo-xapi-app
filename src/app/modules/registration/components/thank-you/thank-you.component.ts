d32b4c03d9f92eba2148821a01f4c60430f6e2d7
Ilia <ilya.ivanov6@gmail.com>
2021-07-20 16:43:00 +0200

    VDS-2327: Fix esLint problems for 'thank-you' component

--- src/app/modules/registration/components/thank-you/thank-you.component.ts
+++ src/app/modules/registration/components/thank-you/thank-you.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'vc-thank-you',
   templateUrl: './thank-you.component.html',
-  styleUrls: ['./thank-you.component.scss']
+  styleUrls: [
+    './thank-you.component.scss',
+  ],
 })
-export class ThankYouComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ThankYouComponent {
 }
