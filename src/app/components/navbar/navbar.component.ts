f2fa02788b2c03a6eb286a53ae540fc51f524c82
Ilia <ilya.ivanov6@gmail.com>
2021-07-27 14:27:07 +0200

    Show specific nav-items on 'logged-in' page navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'vc-navbar',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
   ],
 })
 export class NavbarComponent {
+  constructor(public router: Router) {
+  }
 }
