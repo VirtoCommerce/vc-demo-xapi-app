d89f32c28e0fb6b26d47892079a9370fea9f90ca
Pavel Matantsev <matantsev.pn@gmail.com>
2021-07-28 19:21:48 +0300

    Add two-side password matching validation

--- src/app/shared/password-match-validator.ts
+++ src/app/shared/password-match-validator.ts
@@ -1,9 +1,17 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 export function passwordMatchValidator(originalPasswordId: string): ValidatorFn {
   return (confirmedPasswordControl: AbstractControl): ValidationErrors | null => {
-    const password = confirmedPasswordControl.root.get(originalPasswordId)?.value as string;
+    const passwordControl = confirmedPasswordControl.root.get(originalPasswordId);
+    if (passwordControl) {
+      const subscription: Subscription = passwordControl.valueChanges.subscribe(() => {
+        confirmedPasswordControl.updateValueAndValidity();
+        subscription.unsubscribe();
+      });
+    }
     const confirmedPassword = confirmedPasswordControl.value as string;
+    const password = passwordControl?.value as string;
     return (password && confirmedPassword !== password) ? { passwordMatchValidator: true } : null;
   };
 }
