c79b1b320a8fd79b7c36cc564aefcfa6883bb1ac
Ilia <ilya.ivanov6@gmail.com>
2021-07-30 10:38:16 +0200

    Remove redundant grid classes

--- src/app/modules/login/login-form.layout.ts
+++ src/app/modules/login/login-form.layout.ts
@@ -1,25 +1,14 @@
 import { DynamicFormLayout } from '@ng-dynamic-forms/core';
 
 export const LOGIN_FORM_LAYOUT: DynamicFormLayout = {
-  personalInformation: {
-    element: {
-      control: 'row',
-    },
-  },
   password: {
     element: {
       label: 'required',
-    },
-    grid: {
-      host: 'col-24',
     },
   },
   userName: {
     element: {
       label: 'required',
-    },
-    grid: {
-      host: 'col-24',
     },
   },
 };
