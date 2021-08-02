cbb2224c3e2ef1e5737fea593dd1c4a9e15199ef
Aleksandr Vishniakov <av@virtoway.com>
2021-08-02 17:55:19 +0200

    Fix tests

--- src/app/modules/registration/components/personal-information/personal-information.component.spec.ts
+++ src/app/modules/registration/components/personal-information/personal-information.component.spec.ts
@@ -3,9 +3,7 @@ import { provideMockStore } from '@ngrx/store/testing';
 import { PersonalInformationComponent } from './personal-information.component';
 import * as CompanyRegistration from 'src/app/modules/registration/store/company.reducer';
 import * as Countries from 'src/app/store/countries/countries.reducer';
-import { NG_VALIDATORS } from '@angular/forms';
-import { DYNAMIC_VALIDATORS, Validator, ValidatorFactory } from '@ng-dynamic-forms/core';
-import { passwordMatchValidator } from 'src/app/modules/validation/validators/password-match-validator';
+import { ValidationModule } from 'src/app/modules/validation/validation.module';
 
 describe('PersonalInformationComponent', () => {
   let component: PersonalInformationComponent;
@@ -13,6 +11,9 @@ describe('PersonalInformationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        ValidationModule,
+      ],
       providers: [
         provideMockStore({
           initialState: {
@@ -20,20 +21,6 @@ describe('PersonalInformationComponent', () => {
             companyRegistration: CompanyRegistration.initialState,
           },
         }),
-        {
-          provide: NG_VALIDATORS,
-          useValue: passwordMatchValidator,
-          multi: true,
-        },
-        {
-          provide: DYNAMIC_VALIDATORS,
-          useValue: new Map<string, Validator | ValidatorFactory>([
-            [
-              'passwordMatchValidator',
-              passwordMatchValidator,
-            ],
-          ]),
-        },
       ],
       declarations: [
         PersonalInformationComponent,
