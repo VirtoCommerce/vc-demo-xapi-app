6c53246751a17259887f3ef31acea41c2e5b7735
Pavel Matantsev <kvissachaderah@gmail.com>
2021-08-04 11:56:16 +0300

    Edit "non-numeric" validation messages

--- src/app/modules/validation/constants/validation-messages.constants.ts
+++ src/app/modules/validation/constants/validation-messages.constants.ts
@@ -10,7 +10,7 @@ export const passwordPolicyMessages : { [key: string]: string } = {
   PasswordTooShort: `Password must be at least {{ validator.errorParameter }} characters`,
   PasswordRequiresLower: `Passwords must have at least one lowercase ('a'-'z')`,
   PasswordRequiresUpper: `Passwords must have at least one uppercase ('A'-'Z')`,
-  PasswordRequiresNonAlphanumeric: `Passwords must have at least one alphanumeric character`,
+  PasswordRequiresNonAlphanumeric: `Passwords must have at least one non-alphanumeric character`,
   PasswordRequiresDigit: `Passwords must have at least one digit ('0'-'9')`,
   PasswordRequiresUniqueChars: `Passwords must have at least {{ validator.errorParameter }} unique characters`,
   PasswordRequiresDefaultMessage: `Unexpected error`,
