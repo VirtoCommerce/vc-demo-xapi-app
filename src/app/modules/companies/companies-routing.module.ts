04b93aa9ef745eabac7e6ed846a01c5776c0684c
Anton Veretennikov <superiorboroda@gmail.com>
2021-08-09 18:50:22 +0300

    Add routing to edit with id param

--- src/app/modules/companies/companies-routing.module.ts
+++ src/app/modules/companies/companies-routing.module.ts
@@ -1,8 +1,12 @@
+import { CompanyEditComponent } from './components/company-edit/company-edit.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompaniesComponent } from './companies.component';
 
-const routes: Routes = [{ path: '', component: CompaniesComponent }];
+const routes: Routes = [
+  { path: '', component: CompaniesComponent },
+  { path: ':id', component: CompanyEditComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/companies/components/company-edit/company-edit.component.ts
+++ src/app/modules/companies/components/company-edit/company-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
+
   selector: 'vc-company-edit',
+
   templateUrl: './company-edit.component.html',
-  styleUrls: ['./company-edit.component.scss']
+
+  styleUrls: [
+    './company-edit.component.scss',
+  ],
+
 })
-export class CompanyEditComponent implements OnInit {
 
-  constructor() { }
+export class CompanyEditComponent implements OnDestroy {
+  id: string | undefined;
 
-  ngOnInit(): void {
+  unsubscriber = new Subject();
+
+  constructor(private readonly route: ActivatedRoute) {
+    this.route.params.pipe(takeUntil(this.unsubscriber)).subscribe(params => this.id = params['id'] as string);
   }
 
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
 }
+
