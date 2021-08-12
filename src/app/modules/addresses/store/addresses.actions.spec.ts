820d83c65deea12f2a9e5c15a0cee0e16899b2b5
Ilia <ilya.ivanov6@gmail.com>
2021-08-12 14:08:40 +0200

    Fix addresses selector test

--- src/app/modules/addresses/store/addresses.actions.spec.ts
+++ src/app/modules/addresses/store/addresses.actions.spec.ts
@@ -2,6 +2,6 @@ import * as fromAddresses from './addresses.actions';
 
 describe('getAddressess', () => {
   it('should return an action', () => {
-    expect(fromAddresses.getAddressess().type).toBe('[Addresses] Get Addressess');
+    expect(fromAddresses.getAddressess.type).toBe('[Addresses] Get Addressess');
   });
 });
