18d29deba2506c6f3ddd56b1ecd65e0ea9598212
Ilia <ilya.ivanov6@gmail.com>
2021-08-12 13:54:55 +0200

    Add ContactName sorting

--- src/app/models/pageInfo.model.ts
+++
@@ -1,6 +0,0 @@
-export interface PageInfo {
-  endCursor: string | null,
-  hasNextPage: boolean,
-  hasPreviousPage: boolean,
-  startCursor: string | null
-}
--- src/app/modules/addresses/addresses.component.ts
+++ src/app/modules/addresses/addresses.component.ts
@@ -3,7 +3,6 @@ import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Address } from 'src/app/models/address.model';
-import { PageInfo } from 'src/app/models/pageInfo.model';
 import { selectCurrentCustomerOrganization } from 'src/app/store/current-customer/current-customer.selectors';
 import { getAddressess } from './store/addresses.actions';
 import { selectOrganizationAddresses } from './store/addresses.selectors';
@@ -27,7 +26,7 @@ export class AddressesComponent implements OnInit, OnDestroy {
 
   totalCount?: number | null;
 
-  pageInfo?: PageInfo;
+  cursor = '0';
 
   page = 1;
 
@@ -37,21 +36,45 @@ export class AddressesComponent implements OnInit, OnDestroy {
 
   faChevronRight = faChevronRight;
 
+  sortDescending = 'desc';
+
+  sortAscending = 'asc';
+
+  sortDirection = this.sortDescending;
+
   constructor(private readonly store: Store) { }
 
   ngOnInit(): void {
     this.curentCustomerOrganization$.subscribe(value => this.curentCustomerOrganizationId = value?.id);
-    this.loadAddresses(this.curentCustomerOrganizationId!, this.pageSize);
+    const sortigExpression = this.getSortingExpression();
+    this.loadAddresses(this.curentCustomerOrganizationId!, this.pageSize, this.cursor, sortigExpression);
     this.store.select(selectOrganizationAddresses)
       .pipe(takeUntil(this.unsubscriber))
       .subscribe(addresses => {
         this.addresses = addresses?.items;
         this.totalCount = addresses?.totalCount;
-        this.pageInfo = addresses?.pageInfo;
       });
   }
 
-  loadAddresses(id: string, count?: number, cursor?: string, sort?: string): void {
+  loadPage(page: number): void {
+    this.cursor = (page * this.pageSize - this.pageSize).toString();
+    const sortigExpression = this.getSortingExpression();
+    this.loadAddresses(this.curentCustomerOrganizationId!, this.pageSize, this.cursor, sortigExpression);
+    this.page = page;
+  }
+
+  applySorting(): void {
+    this.sortDirection = this.invertSortDirection(this.sortDirection);
+    const sortigExpression = this.getSortingExpression();
+    this.loadAddresses(this.curentCustomerOrganizationId!, this.pageSize, this.cursor, sortigExpression);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
+
+  private loadAddresses(id: string, count?: number, cursor?: string, sort?: string): void {
     this.store.dispatch(getAddressess({
       id,
       count,
@@ -60,14 +83,11 @@ export class AddressesComponent implements OnInit, OnDestroy {
     }));
   }
 
-  loadPage(page: number): void {
-    const cursor = (page * this.pageSize - this.pageSize).toString();
-    this.loadAddresses(this.curentCustomerOrganizationId!, this.pageSize, cursor!);
-    this.page = page;
+  private invertSortDirection(sortDirection: string): string {
+    return sortDirection === this.sortAscending ? this.sortDescending : this.sortAscending;
   }
 
-  ngOnDestroy(): void {
-    this.unsubscriber.next();
-    this.unsubscriber.complete();
+  private getSortingExpression(): string {
+    return `firstName:${this.sortDirection};lastName:${this.sortDirection}`;
   }
 }
