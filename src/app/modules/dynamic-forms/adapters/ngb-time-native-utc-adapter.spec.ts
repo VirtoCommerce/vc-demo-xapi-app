ba5d233ecbe57fee6de7087d86a0c037e81e4f44
Aleksandr Vishniakov <av@virtoway.com>
2021-08-17 17:20:16 +0200

    Fix tests

--- src/app/modules/dynamic-forms/adapters/ngb-time-native-utc-adapter.spec.ts
+++ src/app/modules/dynamic-forms/adapters/ngb-time-native-utc-adapter.spec.ts
@@ -2,11 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { NgbTimeNativeUTCAdapter } from './ngb-time-native-utc-adapter';
 
-describe('NgbTimeNativeUtcAdapterService', () => {
+describe('NgbTimeNativeUtcAdapter', () => {
   let service: NgbTimeNativeUTCAdapter;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        NgbTimeNativeUTCAdapter,
+      ],
+    });
     service = TestBed.inject(NgbTimeNativeUTCAdapter);
   });
 
--- src/app/modules/dynamic-forms/components/dynamic-ng-bootstrap-datetime-picker/dynamic-ng-bootstrap-datetime-picker.component.spec.ts
+++ src/app/modules/dynamic-forms/components/dynamic-ng-bootstrap-datetime-picker/dynamic-ng-bootstrap-datetime-picker.component.spec.ts
@@ -2,8 +2,14 @@ import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbDatepickerModule, NgbTimepickerModule, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDatepickerModule,
+  NgbTimepickerModule,
+  NgbPopoverModule,
+  NgbDateNativeUTCAdapter,
+} from '@ng-bootstrap/ng-bootstrap';
 import { DynamicFormsCoreModule, DynamicFormService } from '@ng-dynamic-forms/core';
+import { NgbTimeNativeUTCAdapter } from '../../adapters/ngb-time-native-utc-adapter';
 import { DynamicDateTimePickerModel } from './dynamic-datetime-picker.model';
 
 import { DynamicNGBootstrapDatetimePickerComponent } from './dynamic-ng-bootstrap-datetime-picker.component';
@@ -31,6 +37,10 @@ describe('DynamicNgBootstrapDatetimePickerComponent', () => {
       ],
       declarations: [
         DynamicNGBootstrapDatetimePickerComponent,
+      ],
+      providers: [
+        NgbDateNativeUTCAdapter,
+        NgbTimeNativeUTCAdapter,
       ],
     })
       .compileComponents()
