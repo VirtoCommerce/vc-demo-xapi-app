51e880085a97203a2e719d99111d81d7cd106a63
Anton Veretennikov <superiorboroda@gmail.com>
2021-08-19 13:48:24 +0300

    Fix tests

--- src/app/modules/core/components/image-uploader/image-uploader.component.spec.ts
+++ src/app/modules/core/components/image-uploader/image-uploader.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ImageUploaderComponent } from './image-uploader.component';
+import { environment } from 'src/environments/environment';
+import { EnvironmentVariables } from 'src/environments/environment.variables';
 
 describe('ImageUploaderComponent', () => {
   let component: ImageUploaderComponent;
   let fixture: ComponentFixture<ImageUploaderComponent>;
 
   beforeEach(async () => {
+    environment.variables = new EnvironmentVariables('');
     await TestBed.configureTestingModule({
-      declarations: [ ImageUploaderComponent ]
+      declarations: [
+        ImageUploaderComponent,
+      ],
+      imports: [
+        HttpClientTestingModule,
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/modules/dynamic-forms/components/dynamic-image-uploader/dynamic-image-uploader.component.spec.ts
+++ src/app/modules/dynamic-forms/components/dynamic-image-uploader/dynamic-image-uploader.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DynamicImageUploaderModel } from './dynamic-image-uploader.model';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { FormGroup } from '@angular/forms';
 
 import { DynamicImageUploaderComponent } from './dynamic-image-uploader.component';
+import { DynamicFormService } from '@ng-dynamic-forms/core';
 
 describe('DynamicImageUploaderComponent', () => {
+  const testModel = new DynamicImageUploaderModel({ id: 'image' });
+  const formModel = [
+    testModel,
+  ];
+  let formGroup: FormGroup;
   let component: DynamicImageUploaderComponent;
   let fixture: ComponentFixture<DynamicImageUploaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DynamicImageUploaderComponent ]
+      declarations: [
+        DynamicImageUploaderComponent,
+      ],
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(DynamicImageUploaderComponent);
+
+        component = fixture.componentInstance;
+      });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DynamicImageUploaderComponent);
-    component = fixture.componentInstance;
+  beforeEach(inject([
+    DynamicFormService,
+  ], (service: DynamicFormService) => {
+    formGroup = service.createFormGroup(formModel);
+
+    component.group = formGroup;
+    component.model = testModel;
+
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
