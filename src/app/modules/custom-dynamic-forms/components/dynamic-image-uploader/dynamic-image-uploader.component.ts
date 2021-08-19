707d064d3f75c9aad36d7177a6a2d7b4d4dee5a8
Aleksandr Vishniakov <av@virtoway.com>
2021-08-19 18:29:16 +0200

    Revert "Fix change detection, format code"
    This reverts commit 0ea5310e7b61de96f11a0ae0e30438c053700077.

--- src/app/modules/custom-dynamic-forms/components/dynamic-image-uploader/dynamic-image-uploader.component.ts
+++ src/app/modules/custom-dynamic-forms/components/dynamic-image-uploader/dynamic-image-uploader.component.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @angular-eslint/no-output-native */
 import { DynamicImageUploaderModel } from './dynamic-image-uploader.model';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { DynamicFormControlComponent, DynamicFormLayout, DynamicFormLayoutService, DynamicFormValidationService }
   from '@ng-dynamic-forms/core';
 import { FormGroup } from '@angular/forms';
+import { ImageUploaderComponent }
+  from 'src/app/modules/custom-forms/components/image-uploader/image-uploader.component';
 
 @Component({
   selector: 'vc-dynamic-image-uploader',
@@ -15,26 +17,22 @@ import { FormGroup } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DynamicImageUploaderComponent extends DynamicFormControlComponent {
-  @Input()
-  group!: FormGroup;
+  @Input() group!: FormGroup;
 
-  @Input()
-  formLayout?: DynamicFormLayout;
+  @Input() formLayout?: DynamicFormLayout;
 
-  @Input()
-  model!: DynamicImageUploaderModel;
+  @Input() model!: DynamicImageUploaderModel;
 
-  @Output()
-  blur: EventEmitter<any> = new EventEmitter();
+  @Output() blur: EventEmitter<any> = new EventEmitter();
 
-  @Output()
-  change: EventEmitter<any> = new EventEmitter();
+  @Output() change: EventEmitter<any> = new EventEmitter();
 
-  @Output()
-  focus: EventEmitter<any> = new EventEmitter();
+  @Output() focus: EventEmitter<any> = new EventEmitter();
+
+  @ViewChild(ImageUploaderComponent) impageUploaderComponent!: ImageUploaderComponent;
 
   constructor(
-    protected layoutService: DynamicFormLayoutService,
+protected layoutService: DynamicFormLayoutService,
     protected validationService: DynamicFormValidationService
   ) {
     super(layoutService, validationService);
