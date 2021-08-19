c4c9b79f816ed21b56d52d2af5c2fca4e8c0c12f
Anton Veretennikov <superiorboroda@gmail.com>
2021-08-19 13:48:24 +0300

    Fix tests

--- src/app/modules/dynamic-forms/components/dynamic-ngx-codemiror/dynamic-ngx-codemiror.component.spec.ts
+++ src/app/modules/dynamic-forms/components/dynamic-ngx-codemiror/dynamic-ngx-codemiror.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DynamicNgxCodemirorModel } from './dynamic-ngx-codemiror.model';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { DynamicNgxCodemirorComponent } from './dynamic-ngx-codemiror.component';
+import { DynamicFormService } from '@ng-dynamic-forms/core';
+import { FormGroup } from '@angular/forms';
 
 describe('DynamicNgxCodemirorComponent', () => {
+  const testModel = new DynamicNgxCodemirorModel({ id: 'html' });
+  const formModel = [
+    testModel,
+  ];
+  let formGroup: FormGroup;
   let component: DynamicNgxCodemirorComponent;
   let fixture: ComponentFixture<DynamicNgxCodemirorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DynamicNgxCodemirorComponent ]
+      declarations: [
+        DynamicNgxCodemirorComponent,
+      ],
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(DynamicNgxCodemirorComponent);
+
+        component = fixture.componentInstance;
+      });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DynamicNgxCodemirorComponent);
-    component = fixture.componentInstance;
+  beforeEach(inject([
+    DynamicFormService,
+  ], (service: DynamicFormService) => {
+    formGroup = service.createFormGroup(formModel);
+
+    component.group = formGroup;
+    component.model = testModel;
+
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
