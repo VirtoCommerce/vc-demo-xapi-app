eaa198621a1f672eb50cabc87e118f7aaaf371a9
Anton Veretennikov <superiorboroda@gmail.com>
2021-08-23 15:34:43 +0300

    VDS-2522 Use the form group validator for the password matching validator (#31)
    * VDS-2522 Use the form group validator for the password matching validator
    
    * Remove debuging code

--- src/app/modules/validation/validators/password-match-validator.ts
+++ src/app/modules/validation/validators/password-match-validator.ts
@@ -1,22 +1,9 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
-export function passwordMatchValidator(originalPasswordId: string): ValidatorFn {
-  return (confirmedPasswordControl: AbstractControl): ValidationErrors | null => {
-    const passwordControl = confirmedPasswordControl.root.get(originalPasswordId);
-    if (passwordControl) {
-      const unsubscriber = new Subject();
-      passwordControl.valueChanges
-        .pipe(takeUntil(unsubscriber))
-        .subscribe(() => {
-          confirmedPasswordControl.updateValueAndValidity();
-          unsubscriber.next();
-          unsubscriber.complete();
-        });
-    }
-    const confirmedPassword = confirmedPasswordControl.value as string;
-    const password = passwordControl?.value as string;
-    return (password && confirmedPassword !== password) ? { passwordMatchValidator: true } : null;
+export function passwordMatchValidator(): ValidatorFn {
+  return (groupControl: AbstractControl): ValidationErrors | null => {
+    const confirmedPassword = groupControl.get('confirmPassword');
+    const password = groupControl.get('password');
+    return (password?.value !== confirmedPassword?.value) ? { passwordMatchValidator: true } : null;
   };
 }
