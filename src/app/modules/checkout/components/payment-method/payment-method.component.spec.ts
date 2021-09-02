fcbb6b723aaf21c382e0449d5591922188159d3e
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-09-02 10:24:23 +0200

    PT-3667 tests

--- src/app/modules/checkout/components/payment-method/payment-method.component.spec.ts
+++ src/app/modules/checkout/components/payment-method/payment-method.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
+import * as Cart from 'src/app/store/cart/cart.reducer';
 import { PaymentMethodComponent } from './payment-method.component';
 
 describe('PaymentMethodComponent', () => {
@@ -10,6 +11,13 @@ describe('PaymentMethodComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         PaymentMethodComponent,
+      ],
+      providers: [
+        provideMockStore({
+          initialState: {
+            cart: Cart.initialState,
+          },
+        }),
       ],
     })
       .compileComponents();
